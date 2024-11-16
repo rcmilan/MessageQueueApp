28dc3a2698fd2c7e6c6ab75308ae2d399be27e07
rcmilan <13418519+rcmilan@users.noreply.github.com>
2024-11-16 18:08:23 -0300

    MessageQueueAppDbContext

---
+++ MessageQueueApp/Models/UserMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace MessageQueueApp.Models
+{
+    public class UserMessage
+    {
+        public UserMessage(DateTime? createdAt, string content)
+        {
+            CreatedAt = createdAt ?? DateTime.Now;
+            Content = content;
+        }
+
+        public int Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public string Content { get; private set; }
+    }
+}
--- MessageQueueApp/Program.cs
+++ MessageQueueApp/Program.cs
@@ -1,39 +1,45 @@
-
 using MessageQueueApp.Consumers;
+using MessageQueueApp.Database;
+using Microsoft.EntityFrameworkCore;
 
-namespace MessageQueueApp
+namespace MessageQueueApp;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<MessageQueueAppDbContext>(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            options.UseSqlServer(connectionString);
+        });
 
-            // Add services to the container.
-            builder.Services.AddHostedService<MessageConsumer>();
+        // Add services to the container.
+        builder.Services.AddHostedService<MessageConsumer>();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
